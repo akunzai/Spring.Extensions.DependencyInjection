fe1e9db71445ca93fd3dc71149e3f6d4be6dd560
Charley Wu <akunzai@gmail.com>
2021-06-12 00:01:09 +0800

    Simplify samples

--- samples/SampleGenericHostApp/Program.cs
+++ samples/SampleGenericHostApp/Program.cs
@@ -1,7 +1,6 @@
 using System;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using SampleShared;
 using Spring.Context.Support;
 using Spring.Extensions.DependencyInjection;
@@ -12,32 +11,23 @@ namespace SampleGenericHostApp
     {
         public static void Main()
         {
-            var hostBuilder = new HostBuilder()
-            .UseServiceProviderFactory(new SpringServiceProviderFactory(options =>
-            {
-                var context = new CodeConfigApplicationContext();
-                context.ScanAllAssemblies();
-                context.Refresh();
-                options.Parent = context;
-            }))
-            .ConfigureServices(services =>
-            {
-                services.AddLogging(logging => logging.AddConsole());
-                var dummyClock = Environment.GetEnvironmentVariable("SYSTEM_CLOCK");
-                if (!string.IsNullOrWhiteSpace(dummyClock) && DateTime.TryParse(dummyClock, out var dummyDateTime))
+            var host = Host.CreateDefaultBuilder()
+                .UseServiceProviderFactory(new SpringServiceProviderFactory(options =>
                 {
-                    services.AddSingleton<ISystemClock>(_ => new DummySystemClock(dummyDateTime));
-                }
-            });
+                    var context = new CodeConfigApplicationContext();
+                    context.ScanWithTypeFilter(t => t.Name.EndsWith("SpringConfiguration"));
+                    context.Refresh();
+                    options.Parent = context;
+                })).ConfigureServices((context, services) =>
+                {
+                    if (context.Configuration["SystemClock"]?.Equals("Dummy", StringComparison.OrdinalIgnoreCase) == true)
+                    {
+                        services.AddSingleton<ISystemClock, DummySystemClock>();
+                    }
+                }).Build();
 
-            using (var host = hostBuilder.Build())
-            {
-                var clock = host.Services.GetRequiredService<ISystemClock>();
-                Console.WriteLine($"Current DateTime is {clock.Now:O}");
-            }
-
-            Console.WriteLine("Press any key to exit ...");
-            Console.ReadKey();
+            var clock = host.Services.GetRequiredService<ISystemClock>();
+            Console.WriteLine($"Current DateTime is {clock.Now:O}");
         }
     }
 }
